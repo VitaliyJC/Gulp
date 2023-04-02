e4514c8926f16b2a4f4a9c37f0550a74b0751969
Vitaliy Belousov <vitaliy.jc@yandex.ru>
2023-04-02 15:56:13 +0500

    add fonts and pic

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,21 @@ exports.styles = tasks.styles;
 exports.html = tasks.html;
 exports.watch = tasks.watch;
 exports.live_html = tasks.live_html;
+exports.pic = tasks.pic;
+exports.webp = tasks.webp;
+exports.ttf = tasks.ttf;
+exports.ttf2 = tasks.ttf2;
+exports.fonts = tasks.fonts;
 
 
 exports.default = gulp.parallel(
   exports.scripts,
   exports.styles,
+  exports.ttf,
+  exports.ttf2,
+  exports.fonts,
+  exports.pic,
+  exports.webp,
   exports.html,
   exports.live_html,
   exports.watch,
--- tasks/styles.js
+++ tasks/styles.js
@@ -5,18 +5,15 @@ const autoprefixer = require("gulp-autoprefixer");
 const concat = require("gulp-concat");
 const clean = require("gulp-clean-css");
 const scss = require("gulp-sass")(require("sass"));
-const map = require("gulp-sourcemaps");
 const bs = require('browser-sync');
 
 module.exports = function styles() {
   return src("src/style/**/*.scss")
-  .pipe(map.init())
   .pipe(bulk())
   .pipe(scss({ outputStyle: "compressed" }))
   .pipe(autoprefixer({ overrideBrowserslist: ["last 10 version"] }))
   .pipe(clean())
   .pipe(concat("style.min.css"))
-  .pipe(map.write("../sourcemaps/"))
   .pipe(dest("build/css"))
   .pipe(bs.stream());
 }
